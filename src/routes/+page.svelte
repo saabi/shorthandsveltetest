a0a5fecf022b7712311cd8be955bf5aa7ddd079c
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-10-11 01:40:42 -0300

    post

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+	import {onMount} from 'svelte';
+
+	onMount( () => {
+		window.postMessage('hello', '*')
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
