b1f97db35e5f6da5217565e780bdb51105bcaad6
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-10-11 02:02:48 -0300

    reactive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,27 @@
 <script>
 	import {onMount} from 'svelte';
 
+	let message;
+
 	onMount( () => {
 		console.log('posting...')
 		parent.postMessage('hello', '*')
 		console.log('posted')
 
-		addEventListener('message', e => {
-			console.log('received');
-			var key = e.message ? "message" : "data";
-			var data = e[key];
-			console.log(e, key, data)
-		}, false);
+		addEventListener(
+			'message',
+			e => {
+				console.log('received');
+				var key = e.message ? "message" : "data";
+				var data = e[key];
+				message = data;
+			},
+			false
+		);
 	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<pre>
+{message && JSON.stringify(message, null, 2)}
+</pre>
