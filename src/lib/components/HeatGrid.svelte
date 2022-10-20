32c0976966003aecb2906f65f561aa42c31d67ab
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-10-20 00:48:25 -0300

    Added HeatGrid component

---
+++ src/lib/components/HeatGrid.svelte
@@ -0,0 +1,69 @@
+<script>
+	import {createEventDispatcher} from 'svelte';
+
+	export let data;
+	export let rowHeaders;
+	export let columnHeaders;
+	export let colorMap;
+
+	const dispatch = createEventDispatcher();
+
+	const onPointHovered = payload => {
+		dispatch('pointhovered', payload);
+	}
+
+	$: data = data || [];
+	$: rowHeaders = rowHeaders || [];
+	$: columnHeaders = columnHeaders || [];
+	$: colorMap = colorMap || [];
+</script>
+
+<table>
+	<tr>
+		<td>\</td>
+		{#each columnHeaders as columnName}
+			<td>{columnName}</td>
+		{/each}
+	</tr>
+	{#each rowHeaders as rowName, i}
+		<tr>
+			<td>{rowName}</td>
+			{#each columnHeaders as columnName, j}
+				{@const value = data[i][j]}
+				{@const onEnter = () => onPointHovered({columnName, rowName, value})}
+				{@const onExit = () => onPointHovered()}
+				<td>
+					<div
+						class='datapoint'
+						on:blur={onExit}
+						on:focus={onEnter}
+						on:mouseover={onEnter}
+						on:mouseout={onExit}
+						style='background: {colorMap[value]}'
+					/>
+				</td>
+			{/each}
+		</tr>
+	{/each}
+</table>
+
+<style>
+	table {
+		width: 100%;
+		height: 100%;
+	}
+
+	td {
+		text-align: center;
+	}
+
+	td:first-child {
+		text-align: right;
+	}
+
+	.datapoint {
+		width: 16px;
+		height: 16px;
+		margin: auto;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import {onMount} from 'svelte';
 
+	import HeatGrid from '$lib/components/HeatGrid.svelte';
+
 	let message;
+	let datapoint
 
 	onMount( () => {
 		console.log('posting...')
@@ -21,7 +24,38 @@
 	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<pre>
-{message && JSON.stringify(message, null, 2)}
-</pre>
+<div>
+	<HeatGrid
+		columnHeaders={['column 1', 'column 2', 'column 3', 'column 4']}
+		rowHeaders={['row 1', 'row 2', 'row 3']}
+		data={[
+			[0, 1, 2, 3],
+			[1, 2, 3, 0],
+			[2, 3, 0, 1],
+		]}
+		colorMap={[
+			'silver',
+			'cyan',
+			'yellow',
+			'red'
+		]}
+		on:pointhovered={({detail}) => datapoint = detail}
+	/>
+	{#if datapoint}
+		<pre>{JSON.stringify(datapoint, null, 2)}</pre>
+	{/if}
+</div>
+
+<style>
+	div {
+		font-family: sans-serif;
+		width: 800px;
+		height: 150px;
+		display: grid;
+		grid-template-columns: 50% 50%;
+	}
+	pre {
+		background: #eee;
+		border: thin solid silver;
+	}
+</style>
