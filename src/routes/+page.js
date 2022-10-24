82edfe82d5688195fd8284db72ef617d23881df6
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-10-23 22:55:17 -0300

    updated routes

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -4,8 +4,5 @@ import {articleURL} from '$lib/config';
 import {isDev} from '$lib/env';
 
 export function load () {
-	if (!isDev) {
-		throw redirect(301, articleURL);
-	}
-	return {};
+	throw redirect(301, isDev ? '/test' : articleURL);
 }
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import {Slide1, Slide2} from '$lib/components/slides.js';
+</script>
+
+<div>
+	<Slide1 />
+<div>
+
+</div>
+	<Slide2 />
+</div>
